6fe5da1d3109bccd59b76061568d6165b4e85611
ojayyezzir <ojayyezzir@gmail.com>
2019-11-20 13:13:47 +0900

    팔로우 리스트 페이징 진행중
    .

--- src/main/java/com/awethumb/profile/service/ProfileService.java
+++ src/main/java/com/awethumb/profile/service/ProfileService.java
@@ -3,6 +3,7 @@ package com.awethumb.profile.service;
 import java.util.List;
 
 import com.awethumb.repository.vo.Category;
+import com.awethumb.repository.vo.Follow;
 import com.awethumb.repository.vo.Subscribe;
 import com.awethumb.repository.vo.UserVO;
 
@@ -14,4 +15,5 @@ public interface ProfileService {
 	public int checkSub(Subscribe sub);
 	public void deleteSub(Subscribe sub);
 	public void insertSub(Subscribe sub);
+	public List<UserVO> getFollowingList(Follow fol);
 }
--- src/main/webapp/WEB-INF/resources/js/profile/followmod.js
+++ src/main/webapp/WEB-INF/resources/js/profile/followmod.js
@@ -4,6 +4,8 @@
 const fModal = document.querySelector(".f_mod");
 const $fCnt = $(".f_cnt");
 const $followingCon = $(".following_container")
+let currentPageNo = 0;
+const countPerPage = 2;
 
 // 모달창 클래스 토글 기능
 function hideFModal() {
@@ -18,11 +20,10 @@ $fCnt.click((e) => {
             break;
         case "Followers":
             $followingCon.css("z-index", "20");
-                    break;
-            }
- 
-            hideFModal();
-        })
+            break;
+    }
+    hideFModal();    
+})
         // 모달창 밖에 클릭시 모달창 닫힘
 document.querySelector(".f_mod_ol").addEventListener("click", hideFModal)
 
@@ -41,3 +42,44 @@ $(".following_follower_btn").click(() => {
 $(".follower_following_btn").click(() => {
     $followingCon.css("z-index", "21");
 })
+
+function getFollowingListAjax() {
+	// 팔로잉 리스트를 불러오는 ajax
+	$.ajax({
+		url: "getfollowinglist.do",
+		type: "POST",
+		contentType: "application/json",
+		data: JSON.stringify({
+			userNo,
+			currentPageNo
+		})
+	})
+	.done((e) => {
+		currentPageNo += countPerPage;
+		for (let user of e){
+			$(".following_ul").append(
+				`<li class="following_li">
+					<div class="f_mod_img_con">
+						<img src="./../images/test_user.jpg" class="f_mod_img"/>
+					</div>
+					<div class="f_mod_user_con">
+						<a class="f_user_nick">${user.userNickname}</a>
+					<div class="f_user_name">${user.userName}</div>
+					</div>
+					<div class="f_mod_btn_con" data-userno="${user.userNo}">
+						<button class="sub_btn" type="button">
+						구독
+						<i class="fas fa-plus"></i>
+						</button>
+					</div>
+				</li>`)
+		}
+	})
+}
+
+// 최초 1회 실행
+getFollowingListAjax()
+
+
+
+

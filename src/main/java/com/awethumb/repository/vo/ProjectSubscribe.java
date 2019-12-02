8bb96bd49eccfa0a9422363f31c84414f85e4fcf
ojayyezzir <ojayyezzir@gmail.com>
2019-12-02 17:00:53 +0900

    db 수정으로 인한 vo 필드 추가
    .

--- src/main/java/com/awethumb/repository/vo/ProjectSubscribe.java
+++ src/main/java/com/awethumb/repository/vo/ProjectSubscribe.java
@@ -1,5 +1,7 @@
 package com.awethumb.repository.vo;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
@@ -9,4 +11,7 @@ public class ProjectSubscribe {
 
     // 프로젝트 번호 
     private int projectNo;
+    
+    // 저장된 날짜
+    private Date savedDate;
 }

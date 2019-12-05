32142f0879a1c21e94c75099efce4018efcbff0b
woo_seok <woo_seok10@nate.com>
2019-12-05 18:08:46 +0900

    FeedPage VO에 검색을 위한 searchWord 추가

--- src/main/java/com/awethumb/repository/vo/FeedPage.java
+++ src/main/java/com/awethumb/repository/vo/FeedPage.java
@@ -4,6 +4,12 @@ import lombok.Data;
 
 @Data
 public class FeedPage {
-	private int pageIndex; // 페이지 시작(번호 초기값 0) 
-	private int pageCount = 5; // 페이지  (0~몇개씩 뽑을건지)
+	// 페이지 시작(번호 초기값 0) 
+	private int pageIndex; 
+	
+	// 페이지  (0~몇개씩 뽑을건지)
+	private int pageCount = 5; 
+
+    // 검색 키워드
+    private String searchWord;
 }
